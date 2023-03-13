897baae2f29201001fb2e98166914234e4dcaa45
pinkishincoloragain <smb1103@gmail.com>
2023-03-13 13:11:29 +0900

    #4 feat: modularised test, now adding DFS func

--- DataStructure/test.js
+++ DataStructure/test.js
@@ -1,27 +1,48 @@
 import MultiLinkedList from "./MultiLikedList.js";
 import MultiwayTree from "./MultiwayTree.js";
 
-const mll = new MultiLinkedList();
+function testMLL() {
+  const mll = new MultiLinkedList();
 
-mll.add(1, null); // add the root node
-mll.add(2, 1); // add a child node to the root
-mll.add(3, 1); // add another child node to the root
-mll.add(4, 2); // add a child node to the first child of the root
-mll.add(5, 3); // add a child node to the second child of the root
+  mll.add(1, null); // add the root node
+  mll.add(2, 1); // add a child node to the root
+  mll.add(3, 1); // add another child node to the root
+  mll.add(4, 2); // add a child node to the first child of the root
+  mll.add(5, 3); // add a child node to the second child of the root
 
-console.log(mll);
+  // Root: 1
+  // 1st child: 2 - Child: 4
+  // 2nd child: 3 - Child: 5
 
-const mwt = new MultiwayTree();
+  mll.traverseDFS(node => console.log(node.value));
 
-mwt.add(1, null); // add the root node
-mwt.add(2, 1); // add a child node to the root
-mwt.add(3, 1); // add another child node to the root
-mwt.add(4, 2); // add a child node to the first child of the root
-mwt.add(5, 3); // add a child node to the second child of the root
+  console.log(mll);
+}
 
-mwt.find(4); // find the node with value 4
+function testMWT() {
+  const mwt = new MultiwayTree();
 
-mwt.traverseDFS(node => console.log(node.value)); // Output: 1 2 4 3 5
-mwt.remove(3); // remove the second child of the root
+  mwt.add(1, null); // add the root node
+  mwt.add(2, 1); // add a child node to the root
+  mwt.add(3, 1); // add another child node to the root
+  mwt.add(4, 2); // add a child node to the first child of the root
+  mwt.add(5, 3); // add a child node to the second child of the root
 
-mwt.traverseDFS(node => console.log(node.value)); // Output: 1 2 4 5
+  // Root: 1
+  // 1st child: 2 - Child: 4
+  // 2nd child: 3 - Child: 5
+
+  mwt.find(4); // find the node with value 4
+
+  mwt.traverseDFS(node => console.log(node.value));
+  mwt.remove(3);
+
+  mwt.traverseDFS(node => console.log(node.value));
+
+  console.log(mwt);
+}
+
+console.log("-------Multi-level Linked List-------");
+testMLL();
+console.log("-----------MultiWay Tree-----------");
+testMWT();
